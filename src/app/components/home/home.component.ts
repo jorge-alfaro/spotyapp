bd68ae1bbd1a6475b85b58945fa890175aea8273
Jorge M. Alfaro Che <jorgm.alfarc@gmail.com>
2020-09-11 23:03:49 -0500

    manejo de errores

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,14 +12,24 @@ export class HomeComponent {
     newReleases: any[] = [];
     loading: boolean;
 
+    error: boolean;
+    messageError: string;
+
   constructor( private spotify: SpotifyService) {
 
     this.loading = true;
+    this.error = false;
 
     this.spotify.getNewReleases()
       .subscribe( (data: any) => {
         this.newReleases = data;
         this.loading = false;
+      }, (errorService) => {
+
+        this.loading = false;
+        this.error = true;
+        console.log(errorService);
+        this.messageError = errorService.error.error.message;
       });
 
   }
